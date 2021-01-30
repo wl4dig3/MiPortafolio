5f63b1fd52400bd6a348f2a7b271f5b6822bcc52
Wladimir Guevara <wladige@gmail.com>
2021-01-30 19:03:00 -0300

    agregando js para scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,6 @@
 const altura = document.body.scrollHeight - window.innerHeight;
 const fondo = document.getElementById('fondo')
+
+window.onscroll = () => {
+    const anchoFondo = (window.pageYOffset / altura) * 800;
+}
